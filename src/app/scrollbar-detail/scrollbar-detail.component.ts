24c909498dcde2373e4cf63e9dca801a67aa0b06
Karim Abdeselam <karim.abdeselam@pragsis.com>
2020-07-06 17:11:12 +0200

    Custo scrollbar

--- src/app/scrollbar-detail/scrollbar-detail.component.ts
+++ src/app/scrollbar-detail/scrollbar-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-scrollbar-detail',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scrollbar-detail.component.scss']
 })
 export class ScrollbarDetailComponent implements OnInit {
+
+  @Input() from = 0;
+  @Input() to;
 
   constructor() { }
 
