42b8d9fe28a58ecc0cdf27e43b2fde980701bae3
Karim Abdeselam <karim.abdeselam@pragsis.com>
2020-07-13 13:31:30 +0200

    Init Smart and Dumb components

--- src/app/components/scrollbar-detail/scrollbar-detail.component.ts
+++ src/app/components/scrollbar-detail/scrollbar-detail.component.ts
@@ -1,45 +1,33 @@
-import { Component, OnInit, Input, HostListener, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, HostListener, ElementRef, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-scrollbar-detail',
   templateUrl: './scrollbar-detail.component.html',
-  styleUrls: ['./scrollbar-detail.component.scss']
+  styleUrls: ['./scrollbar-detail.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ScrollbarDetailComponent implements OnInit {
+export class ScrollbarDetailComponent {
 
-  width: number;
-  realTo: number;
-  left: number;
+  @Output()
+  mouseover: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  mouseover: boolean;
-
+  
   @HostListener('mouseover')
   onMouseOver() {
-    this.mouseover = true;
+    this.mouseover.emit(true);
   }
 
   @HostListener('mouseout')
   onMouseOut() {
-    this.mouseover = false;
+    this.mouseover.emit(false);
   }
 
-  @Input() from = 0;
-  @Input() to;
+  @Input() from;
   @Input() used;
+  @Input() to;
+  @Input() realTo;
 
   constructor(private el: ElementRef) {
-  }
-
-  ngOnInit() {
-    this.width = this.el.nativeElement.offsetWidth;
-    this.realTo = (this.used * this.width) / this.to;
-    this.left = this.to - this.from - this.used; 
-    console.log(`From --> ${this.from}`)
-    console.log(`Used --> ${this.used}`)
-    console.log(`Left --> ${this.left}`)
-    console.log(`To --> ${this.to}`)
-    console.log(`Width (in px) --> ${this.width}`)
-    console.log(`RealTo = (this.used * this.width) / this.to  (in px) --> ${this.realTo}`)
   }
 
 }
--- src/app/components/scrollbar/scrollbar.component.ts
+++ src/app/components/scrollbar/scrollbar.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-scrollbar',
   templateUrl: './scrollbar.component.html',
-  styleUrls: ['./scrollbar.component.scss']
+  styleUrls: ['./scrollbar.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ScrollbarComponent implements OnInit {
+export class ScrollbarComponent implements OnInit, AfterViewInit {
+  @ViewChild('scrollbar', { static: true }) scrollbar: ElementRef;
 
   from = 0;
   to = 1000;
   used = 815;
 
-  constructor() { }
+  width: number;
+  realTo: number;
+  left: number;
+
+
+  constructor(private cd: ChangeDetectorRef) { }
 
   ngOnInit() {
+    
+    
+    console.log(`From --> ${this.from}`)
+    console.log(`Used --> ${this.used}`)
+    console.log(`Left --> ${this.left}`)
+    console.log(`To --> ${this.to}`)
+    console.log(`Width (in px) --> ${this.width}`)
+    console.log(`RealTo = (this.used * this.width) / this.to  (in px) --> ${this.realTo}`)
+    this.left = this.to - this.from - this.used;
+  }
+  
+  ngAfterViewInit() {
+    // setTimeout(()=>{
+
+      this.width = (this.scrollbar as any).el.nativeElement.querySelector('.fy-c-scrollbar').offsetWidth;
+      this.realTo = (this.used * this.width) / this.to;
+      this.cd.detectChanges();
+
+    // })
   }
 
 }
